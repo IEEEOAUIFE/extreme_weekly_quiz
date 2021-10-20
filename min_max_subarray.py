e2b2efcdc85171be931b3da3b21333a047bb4282
Abdulmalik Bello <adedotunomomeji@gmail.com>
2021-10-20 22:30:30 +0100

    Min_Max_subarray solution

---
+++ min_max_subarray.py
@@ -0,0 +1,8 @@
+def min_max_subarrray(array):
+    sub_lists = []
+    array_max = sorted(array)[-1]
+    array_min = sorted(array)[0]
+    for i in range(len(array) + 1):
+        for j in range(i):
+            sub_lists.append(array[j: i])
+    return min([x for x in lists if (array_max in x) and (array_min in x)],key=len)
