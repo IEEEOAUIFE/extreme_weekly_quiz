ac58eb2bc6468c336417fceaef07be323445346e
Abdulmalik Bello <adedotunomomeji@gmail.com>
2021-10-20 22:32:42 +0100

    Update min_max_subarray.py

--- min_max_subarray.py
+++ min_max_subarray.py
@@ -5,4 +5,4 @@ def min_max_subarrray(array):
     for i in range(len(array) + 1):
         for j in range(i):
             sub_lists.append(array[j: i])
-    return min([x for x in lists if (array_max in x) and (array_min in x)],key=len)
+    return min([x for x in sub_lists if (array_max in x) and (array_min in x)],key=len)
