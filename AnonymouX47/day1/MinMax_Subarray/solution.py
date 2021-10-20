171f64adcc1cdeff96cdf1a12ecd3ac2365f7891
AnonymouX47 <feyidab01@gmail.com>
2021-10-21 00:28:21 +0100

    Added comments; No actual changes to the solution

--- AnonymouX47/day1/MinMax_Subarray/solution.py
+++ AnonymouX47/day1/MinMax_Subarray/solution.py
@@ -1,4 +1,4 @@
-from bisect import bisect, insort
+from bisect import bisect
 
 def min_max(a):
     m, n = 10**9, 1
@@ -9,16 +9,25 @@ def main():
     input()
     arr = [int(x) for x in input().split()]
     x, y = min_max(arr)
+
+    # All elements are the same
     if x == y:
         return 1
 
+    # Actually more efficient than iterating once and appending to separate lists
+    # (by design of the language constructs)
     m = [i for i, e in enumerate(arr) if e == x]
     n = [i for i, e in enumerate(arr) if e == y]
+    del arr  # No longer required
+
+    # Min and Max are different but each occur only once
     if len(m) == 1 == len(n):
         return abs(m[0] - n[0]) + 1
-    del arr
+
+    # Better to iterate through the shorter array since it'll be in linear time
     p = min(m, n, key=len)
     q = n if p is m else m
+    del m, n
 
     l = 5000
     for j in p:
@@ -27,7 +36,7 @@ def main():
             l = min(l, abs(j - q[i-1]) + 1)
         if i < len(q):
             l = min(l, abs(j - q[i]) + 1)
-        if l == 2:
+        if l == 2:  # Can't go any less
             return 2
 
     return l
